6fee24fc3a80dceb5cf966ba2ff976ce4a0b8b9f
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2023-03-07 19:24:53 +0100

    Add Post model Add remote_service.dart to make network call Set up ListView to display posts
    Add http package

---
+++ lib/views/home_page.dart
@@ -0,0 +1,91 @@
+import 'package:api_demo/services/remote_service.dart';
+import 'package:flutter/material.dart';
+import 'package:api_demo/models/post.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
+  var isLoaded = false;
+
+  // load data when page initialises
+  @override
+  void initState() {
+    // fetch data from API
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        child: ListView.builder(
+          itemCount: posts?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                      SizedBox(width: 16,),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        posts![index].title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        posts![index].body ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        replacement: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  getData() async {
+    posts = await RemoteService().getPosts();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+}
